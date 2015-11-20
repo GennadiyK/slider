bb8b823e3f55888bc9e913380a8963d42790e5f4
Henadzi Kalinouski <7742589@gmail.com>
2015-11-20 18:07:12 +0300

    fixed with of slider

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,16 @@
+//1.correct width of ul check width of slider item
+//2.create control rendering for slider
+//3.animate slider
+//4.get data for slider from JSON
+//5.create property - show / hide slider control
+//6. create property for visible count items of slider
+//7.create thumbs
+
 
     function Slider(options) {
         var elem = options.elem;
-        var slider;
+        var slider,sliderList,li,img,imgSrc;
+
 
         //var slider = elem.find('[data-slider="slider"]');
         //var sliderItem = elem.find('[data-slider="item"]');
@@ -18,25 +27,50 @@
 
         function render(){
             slider = document.createElement('div');
-            slider.className = 'slider-wrap';
-            renderItes();
-
+            slider.className = 'slider';
+            renderItems();
         };
 
 
-        function renderItes(){
-            var sliderList = document.createElement('ul');
-                sliderList.className = 'slider-list';
-            var items = options.items || [];
+        function renderItems(){
+            sliderList = document.createElement('ul');
+            sliderList.className = 'slider-list';
+            imgSrc = options.imgSrc || [];
 
-            items.forEach(function(item){
-                var li = document.createElement('li');
-                li.innerHTML = item;
+
+            imgSrc.forEach(function(item,i){
+                li = document.createElement('li');
+                li.className = 'slider-list-item';
+                img = document.createElement('img');
+                img.src = item;
+                $(li).append(img);
                 $(sliderList).append(li);
+
             });
-            $(elem).append(sliderList);
+
+
+
+            $(slider).append(sliderList);
+
+            $(elem).append(slider);
+            fixedSliderWidth('.slider-list-item','.slider-list');
         };
-        //
+
+
+        function renderControl() {
+
+        };
+
+        function fixedSliderWidth(item, container) {
+            var itemElem = $(item);
+            var containerElem = $(container);
+            var itemElemWidth = $(item).outerWidth(true);
+            var containerElemWidth = itemElemWidth * itemElem.length;
+
+            containerElem.width(containerElemWidth);
+        };
+
+
         //elem.on('click', function (e) {
         //    prevNext(e);
         //    toggleClass();
