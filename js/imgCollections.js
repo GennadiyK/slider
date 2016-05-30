862e80e1712ab718ab0b1f840dc80b3f0bbfdc79
Kalinouski  Henadzi <hkalinouski@exadel.com>
2016-05-30 18:16:47 +0300

    reate new slider

---
+++ js/imgCollections.js
@@ -0,0 +1,4 @@
+var imgCollection  = {
+    'one': ['i/i1.jpg', 'i/i2.jpg', 'i/i3.jpg'],
+    'two': ['i22', 'i23']
+};
--- js/slider.js
+++ js/slider.js
@@ -1,156 +1,59 @@
-    function Slider(options) {
-        this.elem = options.elem;
-        this.imgCollection = options.imgCollection || [];
-
-        this._slider = null;
-        this._sliderList = null;
-        this._currentSlide = 0;
-        this._li = null;
-        this._img = null;
-        this._mrg = 0;
+    function Slider() {
+        this.elem = document.querySelectorAll('[data-elem="slider"]');
+        this.imgCollection = [];
     }
-        //final render of slider
-    Slider.prototype.getElem = function(){
-        if(!this._slider) this.render();
-        return this._slider;
+
+    Slider.prototype.render = function() {
+        this.getImgCollection();
+        for(var i=0; i<this.elem.length; i++) {
+            this.elem[i].appendChild(this.generateListEl(this.imgCollection[i]));
+        }
     };
 
-        //rendering slider-list(ul)
-    Slider.prototype.render = function(){
-        this._slider = document.createElement('div');
-        this._slider.className = 'slider';
-        this.renderItems();
-        this.renderControl();
-        this.clickOnElement();
-    };
-
-        /*creating ul.slider-list
-         creating  li.slider-list-item,
-         append all into the elem,
-         add current class for firs li,
-         correct slider width - fixedSliderWidth()*/
-
-    Slider.prototype.renderItems = function(){
-        this._sliderList = document.createElement('ul');
-        this._sliderList.className = 'slider-list';
-
-        this.imgCollection.forEach(function(item){
-            this._li = document.createElement('li');
-            this._li.className = 'slider-list-item';
-            this._img = document.createElement('img');
-            this._img.src = item;
-            $(this._li).append(this._img);
-            $(this._sliderList).append(this._li);
-
-        },this);
-
-        $(this._slider).append(this._sliderList);
-        $(this.elem).append(this._slider);
-        $('.slider-list-item:first-child').addClass('current');
-
-        this.fixedSliderWidth('.slider-list-item','.slider-list');
-    };
-
-        //creating control for slider
-    Slider.prototype.renderControl = function() {
-        var prev = document.createElement('a');
-        var next = document.createElement('a');
-
-        prev.setAttribute('class','slider-control');
-        next.setAttribute('class','slider-control');
-        prev.setAttribute('data-control','prev');
-        next.setAttribute('data-control','next');
-        prev.setAttribute('href','#');
-        next.setAttribute('href','#');
-
-        prev.innerHTML = 'prev';
-        next.innerHTML = 'next';
-
-        $(this.elem).append(prev,next);
-    };
-
-        //correcting full width of slider
-    Slider.prototype.fixedSliderWidth = function (item, container) {
-        var itemElem = $(item);
-        var containerElem = $(container);
-        var itemElemWidth = $(item).outerWidth(true);
-        var containerElemWidth = itemElemWidth * itemElem.length;
-
-        containerElem.width(containerElemWidth);
-    };
-
-        //delegation click event on control buttons and apply prevNext() function
-    Slider.prototype.clickOnElement = function() {
-
-        this.elem.on('click', function (e) {
-            this.prevNext(e);
-            this.toggleClass('.slider-list','.slider-list-item','current');
-        }.bind(this));
-    };
-
-
-        //checking event.target and if click was on prev button - run prev(); if click was on next - run next()
-    Slider.prototype.prevNext= function (e) {
-        e.preventDefault();
-
-        var eventElem = e.target;
-
-        if (!$(eventElem).data("control")) return;
-
-        if ($(eventElem).data("control") == 'next') {
-            this.next();
-        } else if ($(eventElem).data("control") == 'prev') {
-            this.prev();
+    Slider.prototype.getDataImg = function() {
+        var dataImg = [];
+        for(var i = 0; i < this.elem.length; i++) {
+            dataImg[i] = this.elem[i].dataset.img;
         }
 
+       return dataImg;
     };
 
-        //move ul with margin-left
-    Slider.prototype.next = function() {
-        if (this._currentSlide < this.imgCollection.length-1) {
-            this._currentSlide++;
-
-            if(this._currentSlide%2 != 0) {
-                this._mrg += $(this._li).outerWidth(true);
-                $(this._sliderList).css({
-                    'margin-left': '-' + this._mrg + 'px'
-                });
+    Slider.prototype.getImgCollection = function() {
+        var dataImg = this.getDataImg();
+        for(var i =0; i < dataImg.length; i++) {
+            switch(dataImg[i]) {
+                case 'one':
+                    this.imgCollection[i] = imgCollection.one;
+                    break;
+                case 'two':
+                    this.imgCollection[i] = imgCollection.two;
+                    break;
             }
-
-        } else {
-            this._currentSlide = 0;
-            this._mrg = 0;
-            $(this._sliderList).css({
-                'margin-left': this._mrg + 'px'
-            });
         }
     };
 
-        //move ul with margin-left
-    Slider.prototype.prev = function() {
-        if (this._currentSlide != 0) {
-            this._currentSlide--;
-            if(this._currentSlide%2 == 0 ) {
-                this._mrg -= $(this._li).outerWidth(true);
-                $(this._sliderList).css({
-                    'margin-left': '-' + this._mrg + 'px'
-                });
-            }
-        } else {
-            this._currentSlide = this.imgCollection.length-1;
-            this._mrg = $(this._sliderList).width() - ($(this._li).outerWidth(true)*3);
-            console.log(this._mrg);
-            $(this._sliderList).css({
-                'margin-left': '-' + this._mrg + 'px'
-            });
+    Slider.prototype.generateListEl = function(collectionImg) {
+        var listContainerEl = document.createElement('ul');
+            listContainerEl.classList.add('slider-list')
+        var listContainerWrapper = document.createElement('div');
+            listContainerWrapper.classList.add('slider-list-wrapper');
+
+        for(var i = 0; i < collectionImg.length; i++) {
+            var liEl = document.createElement('li');
+                liEl.classList.add('slider-list-item');
+            var image = document.createElement('img');
+                image.src = collectionImg[i];
+                image.classList.add('slider-list-img');
+                liEl.appendChild(image);
+
+            listContainerEl.appendChild(liEl);
+            listContainerWrapper.appendChild(listContainerEl);
         }
+
+        return listContainerWrapper;
     };
 
-        //changing class on li element with currentSlide element index
-    Slider.prototype.toggleClass = function (parent,item,className) {
-        $(parent).find('.'+className).removeClass(className);
-        $(item).eq(this._currentSlide).addClass(className);
+    Slider.prototype.setSlideWidth = function() {
+
     };
-
-
-
